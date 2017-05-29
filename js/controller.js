c984ced645ddd6755f703dc7a139b0d4808d4d92
sc-aug <shichuanwang@yahoo.com>
2017-05-29 15:47:44 -0500

    rm redundant code

--- js/controller.js
+++ js/controller.js
@@ -51,48 +51,10 @@ var Controller = {
     View.pin(st, ds);
   },
 
-  // clean: function() {
-  //   Model.clean();
-  //   Controller.refreshBlocks();
-  // },
-
   addRandNode: function() {
     var xyPairs = Util.genRandNode();
     Model.addRandNode(xyPairs);
-  },
-
-  // // mouse click function
-  // flipCellState: function(tdid) {
-  //   var mat = model.getCurMatrix();
-  //   var cord = tdid.split("_");
-  //   var i = cord[1], j = cord[2];
-  //   var state = mat[i][j];
-  //   mat[i][j] = !state;
-  //   if (mat[i][j]) {
-  //     view.live(i,j);
-  //   } else {
-  //     view.die(i,j);
-  //   }
-  // },
-
-  // focusCell: function(tdid) {
-  //   var mat = model.getCurMatrix();
-  //   var cord = tdid.split("_");
-  //   var i = cord[1], j = cord[2];
-  //   var state = mat[i][j];
-  //   view.foc(i,j);
-  // },
-
-  // unFocusCell: function(tdid) {
-  //   var mat = model.getCurMatrix();
-  //   var cord = tdid.split("_");
-  //   var i = cord[1], j = cord[2];
-  //   if (mat[i][j]) {
-  //     view.live(i,j);
-  //   } else {
-  //     view.die(i,j);
-  //   }
-  // }
+  }
 
 };
 
